9656208c9eb7cb949f6cac95309fce5930a4df4b
omar2071-code <omarabdallah2071@gmail.com>
2025-11-06 18:28:27 +0200

    add hive initFlutter and hive openBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_app/screens/edit_note_screen.dart';
 import 'package:note_app/screens/note_screen.dart';
+import 'package:note_app/widgets/constaonts_file.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const NoteApp());
 }
 

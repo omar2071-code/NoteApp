1cb2db51148b1741fc8d6f39035e8d8ccf9a1734
omar2071-code <omarabdallah2071@gmail.com>
2025-11-07 23:23:42 +0200

    change name app and logo icon

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -12,17 +12,48 @@ class NotesListView extends StatelessWidget {
     return BlocBuilder<NotesCubit, NotesState>(
       builder: (context, state) {
         List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes;
-        return ListView.builder(
-          itemCount: notes.length,
-          padding: EdgeInsets.symmetric(vertical: 16),
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: NotesItem(noteModel: notes[index]),
+        //code
+        return notes.length == 0
+            ? EmptyBodyNotes()
+            : ListView.builder(
+              itemCount: notes.length,
+              padding: EdgeInsets.symmetric(vertical: 16),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: NotesItem(noteModel: notes[index]),
+                );
+              },
             );
-          },
-        );
       },
+    );
+  }
+}
+
+class EmptyBodyNotes extends StatelessWidget {
+  const EmptyBodyNotes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: double.infinity,
+        height: 200,
+        decoration: BoxDecoration(
+          color: Color(0xffe5e5e5),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Center(
+          child: Text(
+            'Add Notes +',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
